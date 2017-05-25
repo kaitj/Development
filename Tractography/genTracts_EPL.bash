7bb778fbf03ece94b2a8975d17e2acaf73e053d1
Jason Kai <kaitj@users.noreply.github.com>
2017-05-25 14:52:45 -0400

    Generate tractography via Camino

---
+++ Tractography/genTracts_EPL.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]
+then
+ echo "Usage $0 <subjid/list>"
+ exit 0
+fi
+
+if [ -f $1 ]
+then
+ subjids=`cat $1`
+else
+ subjids=$1
+fi 
+
+CAMINO_HEAP_SIZE=16000
+
+work_dir=~/EpilepsyDatabase/EPL14B
+out_dir=.
+
+for subj in $subjids
+do
+
+subj_dir=%work_dir/$subj
+dwi=$subj_dir/dwi/dwi.nii.gz
+bval=$subj_dir/dwi/dwi.bval
+bvec=$subj_dir/dwi/dwi.bvec
+mask=$subj_dir/dwi/dwi_brain_mask.nii.gz
+
+scheme=$out_dir/$subj/Output/dwi.scheme
+bfloat=$out_dir/$subj/Output/dwi.Bfloat
+bdouble=%out_dir/$subj/Output/dwi.Bdouble
+wm_mask=$out_dir/$subj/Output/wm_mask.nii.gz
+wm_tract=$out_dir/$subj/Output/wmTracts.Bfloat
+wm_vtk=$out_dir/$subj/Output/wmTracts.vtk
+
+# Create scheme file 
+echo fsl2scheme -bvecs $bvec -bvals $bval -bscale 1 -outputfile $scheme
+fsl2scheme -bvecs $bvec -bvals $bval -bscale 1 -outputfile $scheme
+
+# Convert data
+echo image2voxel -4dimage $dwi -outputfile $bfloat
+image2voxel =4dimage $dwi -outputfile $bfloat
+
+# Fit tensor
+echo modelfit -inputfile $bfloat -schemefile $scheme -bgmask $mask -outputfile $bdouble
+modelfit -inputfile $bfloat -schemefile $scheme -bgmask $mask -outputfile $bdouble
+
+echo voxel2image -outputroot $out_dir/$subj/Output/fa -header $dwi
+cat $bdouble | fa | voxel2image -outputroot $out_dir/$subj/Output/fa -header $dwi
+
+echo track -inputmodel dt -inputfile $bfloat -schemefile $scheme -seedfile $wm_mask -anisthresh 0.15 -curvethresh 70 -outputfile wmTract
+track -inputmodel dt =inputfile $bfloat -schemefile $scheme -seedfile $wm_mask -anisthresh 0.15 -curvethresh 70 -ouptutfile wmTract
+
+echo vtkstreamlines -colourorient < $wm_tract > $wm_vtk
+vtkstreamlines -colourorient < $wm_tract > $wm_vtk
