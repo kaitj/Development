6942df0d1155324ee0082163fda20f886ce21060
kaitj <tkai@uwo.ca>
2018-08-27 14:09:00 -0400

    new install scripts

--- install_scripts/05_install_neurobeer.sh
+++ install_scripts/05_install_neurobeer.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-DIR=/opt/git/
+DIR=/opt/git
 mkdir -p $DIR/neurobeer
+
+# Install missing dependencies
+apt-get install -y libsm6 libxt6 libgl1-mesa-glx
+apt-get install -y python3-tk
 
 # Git
 git clone https://github.com/kaitj/neurobeer $DIR/neurobeer
 cd $DIR/neurobeer
+git checkout khanlab # dev branch, remove for release
 
 # Install requirements
 pip3 install -r requirements.txt
